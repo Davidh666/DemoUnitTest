0584f1574bb7e1e33056f411359b72fbca035a03
Davidh666 <jdherrera0.2.0@gmail.com>
2026-09-09 18:21:02 -0400

    Tarea 3

---
+++ DemoBiblioteca/Abstract.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoBiblioteca
+{
+    public abstract class Figura
+    {
+        public abstract decimal area();
+        public abstract decimal perimetro();
+    }
+    public class Cuadrado : Figura
+    {
+        public readonly decimal lado;
+
+        public Cuadrado(decimal lado) { this.lado = lado; }
+
+        public override decimal area()
+        {
+            return lado * lado;
+        }
+        public override decimal perimetro()
+        {
+            return lado * 4;
+        }
+    }
+    public class Rentangulo : Figura
+    {
+        public readonly decimal lado;
+
+        public Rentangulo(decimal lado) => this.lado = lado;
+
+        public override decimal area()
+        {
+            throw new NotImplementedException();
+        }
+        public override decimal perimetro()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DemoPruebaUnitaria/Test1.cs
@@ -0,0 +1,27 @@
+﻿using DemoBiblioteca;
+
+namespace DemoPruebaUnitaria
+{
+    [TestClass]
+    public sealed class TesTestUnitCuadrado
+    {
+        [TestMethod]
+        public void AreaCuadrado()
+        {
+            var Cuadrado = new Cuadrado(5);
+            var resultado = Cuadrado.area();
+            Assert.AreEqual(25, resultado);
+        }
+    }
+
+    [TestClass]
+    public sealed class TestUnitRectangulo
+    {
+        [TestMethod]
+        public void Rectangulo()
+        {
+
+        }
+    }
+}
+
